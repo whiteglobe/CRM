7e03af9d300ed8e28a438577cafdf97e4172c4a8
whiteglobe <sales.whiteglobe@gmail.com>
2019-08-17 15:05:59 +0530

    project crm

--- app/src/main/java/com/whiteglobe/crm/Dashboard.java
+++ app/src/main/java/com/whiteglobe/crm/Dashboard.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -13,13 +14,20 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class Dashboard extends AppCompatActivity {
 
     FloatingActionButton userAccount,logout,leads,meetings;
     SharedPreferences sessionDashboard;
     boolean doubleBackToExitPressedOnce = false;
+    private static final String TAG = "Dashboard";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,10 @@ public class Dashboard extends AppCompatActivity {
         toolbar.setTitle(sessionDashboard.getString("username",null));
         //Toast.makeText(getApplicationContext(),sessionDashboard.getString("uname",null), Toast.LENGTH_SHORT).show();
 
+        //Firebase Token
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+        firebaseToken();
+
         //To go in User Account Activity
         userAccount();
 
@@ -44,6 +56,28 @@ public class Dashboard extends AppCompatActivity {
 
         //To go in Meetings Activity
         meetings();
+    }
+
+    private void firebaseToken()
+    {
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = getString(R.string.fcm_token, token);
+                        Log.d(TAG, msg);
+                        Toast.makeText(Dashboard.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void userAccount()
---
+++ app/src/main/java/com/whiteglobe/crm/MyFirebaseMessagingService.java
@@ -0,0 +1,43 @@
+package com.whiteglobe.crm;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        super.onMessageReceived(message);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+                .setContentTitle(message.getNotification().getTitle())
+                .setContentText(message.getNotification().getBody())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d("Token", "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(token);
+    }
+}
